3e4f735ca2e3139164db3c3d0d2ff23895254272
ryo121w <ryoutarou9121@icloud.com>
2023-09-15 12:14:36 +0900

    commit

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Start Gunicorn processes
-echo Starting Gunicorn.
-cd mybackend
-gunicorn mybackend.wsgi:application --bind 0.0.0.0:$PORT --log-file -
-
-# Start React development server
-echo Starting React Dev Server
-cd ../frontend
-PORT=$((PORT + 1)) npm start
+cd mybackend && gunicorn mybackend.wsgi:application --bind 0.0.0.0:$PORT --log-file - &
+cd frontend && npm start
