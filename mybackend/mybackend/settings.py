3c7d2be1dbfbfa6ba85a0d83d36f7c84aebb8e33
ryo121w <ryoutarou9121@icloud.com>
2023-09-15 19:04:56 +0900

    commit

--- mybackend/mybackend/settings.py
+++ mybackend/mybackend/settings.py
@@ -91,7 +91,7 @@ ROOT_URLCONF = "mybackend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [os.path.join(BASE_DIR, 'mybackend/staticfiles')],
+        'DIRS': [os.path.join(BASE_DIR, 'mybackend/static')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cd mybackend && gunicorn mybackend.wsgi:application --bind 0.0.0.0:$PORT --log-file -
+cd mybackend
+python manage.py collectstatic --noinput  # 静的ファイルを集める
+gunicorn mybackend.wsgi:application --bind 0.0.0.0:$PORT --log-file -
